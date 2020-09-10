f78d6cec45ae4142a59929886415d510a039da08
Finley Sherwood <me@itsf.in>
2020-09-10 17:00:12 +1000

    Initialise node.js applications and load events/commands

---
+++ index.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
+client.config = require('./config.json')
+
+require('dotenv').config()
+const { glob } = require('glob')
+const { parse } = require('path')
+
+// Load events
+glob('./events/*.js', (_, files) => {
+    files.forEach(file => {
+        const { name } = parse(file)
+        
+        const event = require(file)
+        
+        client.on(name, event.bind(null, client))
+    })
+})
+
+// Load commands
+client.commands = new Discord.Collection()
+glob('./commands/**/*.js', (_, files) => {
+    files.forEach(file => {
+        const { dir, name } = parse(file)
+        const category = dir.split('/').pop()
+        
+        const command = require(file)
+        command.category = category
+        command.name = name
+        
+        client.commands.set(name, command)
+    })
+})
+
+// Login
+client.login(process.env.token)
