c87b148d0d47dec972fc2b7baadaf3e83149d346
Finley Sherwood <me@itsf.in>
2020-09-16 15:59:18 +1000

    Add recent command

---
+++ commands/tournaments/recent.js
@@ -0,0 +1,50 @@
+module.exports = {
+    desc: 'lists recent tournaments',
+	syntax: '',
+	async run(message, args) {
+        const Discord = require('discord.js')
+        const moment = require('moment-timezone')
+        const momentDurationFormatSetup = require("moment-duration-format")
+
+        const events = await message.client.sql('SELECT * FROM `tournaments` WHERE `ttime`>'+(moment().unix()-604800)+' AND `ttime`<'+moment().unix())
+        events.sort(function(a, b) {return b.ttime - a.ttime})
+
+        const userSQL = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.author.id+'"')
+        const timezone = userSQL[0].timezone
+
+        const Embed = new Discord.MessageEmbed()
+            .setColor(message.client.config.color)
+            .setTitle('Recent Tournaments')
+            .setURL('https://orla.pro')
+            .setDescription(`These are the tournaments run over the past week, adjusted for **${timezone.split('/')[1]}** time.`)
+            .setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+        
+        for (const event of events) {
+            if (message.client.hosts[event.host] !== undefined) {
+                const icon = message.client.emojis.cache.get(message.client.hosts[event.host].emoji)
+                const time = moment.unix(event.ttime).tz(timezone).format('dddd Do MMMM, h:mma z')
+
+                let rtime = ''
+                if (moment.unix(event.rtime).tz(timezone).format('h:mm') !== moment.unix(event.ttime).tz(timezone).format('h:mm')) {
+                    rtimeFormat = (moment.unix(event.rtime).tz(timezone).format('dddd') === moment.unix(event.ttime).tz(timezone).format('dddd')) ? 'h:mma z' : 'dddd Do MMMM, h:mma z'
+                    rtime = `\nRego Closes:** ${moment.unix(event.rtime).tz(timezone).format(rtimeFormat)} **`
+                }
+
+                const topen = (event.topen === 1) ? 'Yes' : 'No'
+                const stream = (event.stream === null) ? '' : `\nStream:** ${event.stream} **`
+
+                const content = `Time: **${time}**`
+                                +rtime
+                                +`\nOpen: **${topen}**`
+                                +`\nMode: **${event.mode}**`
+                                +`\nPrize: **${event.prize}**`
+                                +stream
+                                +`\nURL:** ${event.link} **`
+
+                Embed.addField(`${icon} **__${event.title}__**`, content)
+            }
+        }
+
+        message.channel.send(Embed)
+    }
+}
