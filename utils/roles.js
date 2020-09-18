817e0a10cd0ee35bfccb4adce86a3aa3eda3f24e
Finley Sherwood <me@itsf.in>
2020-09-18 14:56:07 +1000

    Mutually exclusive reaction role

--- utils/roles.js
+++ utils/roles.js
@@ -1,25 +1,31 @@
 module.exports = {
     async addStateRole(client, reaction, user) {
-        let getRoleID = await client.sql('SELECT * FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
-        getRoleID = getRoleID[0].getrole
-
-        if (reaction.message.id === getRoleID) {
-            const states = await client.sql('SELECT * FROM `states`')
-            console.log(states)
+        const server = await client.sql('SELECT * FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
+        const getrole = server[0].getrole
+        const states = JSON.parse(server[0].states)
+        
+        if (reaction.message.id === getrole) {
             states.forEach(state => {
                 if (reaction._emoji.id == state.emoji) {
+                    reaction.message.reactions.cache.forEach(emote => {
+                        if (emote._emoji.id !== reaction._emoji.id) emote.remove(user.id)
+                    })
+                    states.forEach(state => {
+                        if (reaction.message.guild.member(user.id).roles.cache.some(role => role.id === state.role)) {
+                            reaction.message.guild.member(user.id).roles.remove(state.role)
+                        }
+                    })
                     reaction.message.guild.member(user.id).roles.add(state.role)
                 }
             })
         }
     },
     async removeStateRole(client, reaction, user) {
-        let getRoleID = await client.sql('SELECT * FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
-        getRoleID = getRoleID[0].getrole
+        const server = await client.sql('SELECT * FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
+        const getrole = server[0].getrole
+        const states = JSON.parse(server[0].states)
 
-        if (reaction.message.id === getRoleID) {
-            const states = await client.sql('SELECT * FROM `states`')
-            console.log(states)
+        if (reaction.message.id === getrole) {
             states.forEach(state => {
                 if (reaction._emoji.id == state.emoji) {
                     reaction.message.guild.member(user.id).roles.remove(state.role)
