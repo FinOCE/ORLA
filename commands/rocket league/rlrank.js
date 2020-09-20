b1abb7ec687444938b3f67e4277fe21cbf56ec92
Finley Sherwood <me@itsf.in>
2020-09-21 01:26:04 +1000

    Add rlrank command

---
+++ commands/rocket league/rlrank.js
@@ -0,0 +1,162 @@
+module.exports = {
+	desc: 'checks rocket league rank stats',
+	syntax: '[platform] [account]',
+	async run(message, args) {
+		const Discord = require('discord.js')
+		const moment = require('moment-timezone')
+
+		const requestInitiated = moment().unix()
+
+		const accountInfo = {
+			"isReal": false
+		}
+
+		if (!(0 in args)) {
+			// if checking own rank
+			const userSQL = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.author.id+'"')
+			const user = userSQL[0]
+
+			if (user.account !== null) {
+				// if account is linked
+				accountInfo['platform'] = user.platform
+				accountInfo['account'] = user.account
+				accountInfo['isReal'] = true
+			} else {
+				// if account is not linked
+				message.client.error('notLinked', message)
+				return
+			}
+		} else {
+			// if checking other player's rank
+			if (message.mentions.members.first()) {
+				// if checking via ping
+				const userSQL = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.mentions.members.first().user.id+'"')
+				const user = userSQL[0]
+
+				if (user.account !== null) {
+					// if account is linked
+					accountInfo['platform'] = user.platform
+					accountInfo['account'] = user.account
+				} else {
+					// if account is not linked
+					message.client.error('notLinkedOther', message)
+					return
+				}
+			} else {
+				// if checking via platform and account
+				if (['pc', 'ps', 'xbox'].indexOf(args[0].toLowerCase()) === -1) {
+                    message.client.error('invalidPlatform', message)
+                    return
+                }
+                
+                if (!(1 in args)) {
+                    message.client.error('noPlayerSpecified', message)
+                    return
+				}
+				
+				accountInfo['platform'] = args[0].toLowerCase()
+				accountInfo['account'] = args[1]
+			}
+		}
+
+		const loading = message.guild.emojis.cache.find(emoji => emoji.name === 'd_loading')
+
+		message.channel.send(`${loading} Getting rank...`).then(async msg => {
+			// Don't bother looking for stats.js, its in the .gitignore
+			const stats = require('../../utils/stats.js')
+
+			const playerRaw = await stats(accountInfo['platform'], accountInfo['account'])
+
+			if (playerRaw !== null) {
+				const player = JSON.parse(playerRaw)
+				
+				const Embed = new Discord.MessageEmbed()
+					.setColor(message.client.config.color)
+					.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+					.setTitle(`RL Stats: ${player.user.name}`)
+					.setURL(player.user.statsURL)
+                    .setThumbnail(player.user.avatarURL)
+				
+				let totalModes = 0
+				let topRank = false
+				for (const i in player.modes) {
+					let mode = player.modes[i]
+					
+					if (mode.name !== 'SoloStandard') {
+						const ranksList = ['Unranked','BronzeI','BronzeII','BronzeIII','SilverI','SilverII','SilverIII','GoldI','GoldII','GoldIII','PlatinumI','PlatinumII','PlatinumIII','DiamondI','DiamondII','DiamondIII','ChampionI','ChampionII','ChampionIII','GrandChampion']
+						const ranksEmoji = ['00_unranked','01_bronze1','02_bronze2','03_bronze3','04_silver1','05_silver2','06_silver3','07_gold1','08_gold2','09_gold3','10_plat1','11_plat2','12_plat3','13_diamond1','14_diamond2','15_diamond3','16_champion1','17_champion2','18_champion3','19_grandchampion']
+						let emote = ''
+						for (x = 0; x < ranksList.length; x++) {
+							if (mode.rank.replace(' ','') == ranksList[x]) {
+								emote = message.guild.emojis.cache.find(emoji => emoji.name === ranksEmoji[x])
+							}
+						}
+						
+						d_up = message.guild.emojis.cache.find(emoji => emoji.name === 'd_up')
+						d_down = message.guild.emojis.cache.find(emoji => emoji.name === 'd_down')
+						
+						const rank = ((mode.rank === 'Grand Champion') || (mode.rank === 'Unranked')) ? `${emote} (${mode.mmr})` : `${emote} Div ${mode.division} (${mode.mmr})`
+						
+						let mmr = ''
+						if (mode.rank === 'Grand Champion') {
+							mmr = (mode.down !== undefined) ? `${d_down} ${mode.down}` : ''
+						} else {
+							mmr = ((mode.rank === 'Unranked') || (mode.down === undefined) || (mode.up === undefined)) ? '' : `${d_up} ${mode.up} ${d_down} ${mode.down}`
+						}
+						
+						const streak = (mode.streak > 0) ? `🔥 ${mode.streak}` : `❄️ ${String(mode.streak).replace('-','')}`
+						const games = (mode.name === 'Casual') ? '' : `🕐 ${mode.games} - ${streak}`
+						
+						const position = `(Top ${Math.floor((100 - mode.percentile)*10)/10}%)`
+						
+						Embed.addField(`__${mode.name}__ *${position}*`, `${rank}\n${mmr}\n${games}`, true)
+						
+						totalModes += 1
+						
+						topRank = (mode.rankValue > topRank) ? mode.rankValue : topRank
+					}
+				}
+				
+				while (totalModes % 3 !== 0) {
+					Embed.addField("‎", "‎", true) // these have invisible characters in them
+					totalModes += 1
+				}
+
+				msg.edit(' ', Embed)
+
+				if (topRank !== false && accountInfo['isReal'] === true) {
+					const rankIDSQL = await message.client.sql('SELECT * FROM `servers` WHERE `id`="'+message.channel.guild.id+'"')
+					const rankID = JSON.parse(rankIDSQL[0].ranks)
+					
+					if (rankID !== null) {
+						for (rankNum in rankID) {
+							if (message.member.roles.cache.some(role => role.id === rankID[rankNum])) {
+								message.member.roles.remove(rankID[rankNum])
+							}
+						}
+						
+						message.member.roles.add(rankID[topRank])
+					}
+				}
+			} else {
+				if (requestInitiated > (moment().unix() - 20)) {
+					const Embed = new Discord.MessageEmbed()
+						.setColor(message.client.config.color)
+						.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+						.setTitle('Error: Profile Not Found')
+						.setDescription('The profile you searched for could not be found. Please try again.')
+					
+					msg.edit(' ', Embed)
+				} else {
+					const Embed = new Discord.MessageEmbed()
+						.setColor(message.client.config.color)
+						.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+						.setTitle('Error: Request Timeout')
+						.setDescription('The server took too long to respond. Unfortunately we can\'t receive your account stats right now.')
+					
+					msg.edit(' ', Embed)
+				}
+			}
+		})
+	}
+}
--- utils/error.js
+++ utils/error.js
@@ -1,24 +1,43 @@
-const { DiscordAPIError } = require("discord.js")
-
 module.exports = (method, message) => {
     const Discord = require('discord.js')
     
     // List all error methods and their responses
+    const prefix = message.client.config.prefix
+
     const methods = {
+        // general
         "invalidPermission" : {
             "title": "Invalid Permission",
             "description": "Sorry, but you don't have permission to use that command."
         },
         "invalidSyntax": {
             "title": "Invalid Syntax",
-            "description": `To check the correct syntax, please type \`${message.client.config.prefix}help [command]\``
+            "description": `To check the correct syntax, please type \`${prefix}help [command]\``
+        },
+
+        // .rlrank
+        "notLinked": {
+            "title": "Account Not Linked",
+            "description": `To link your account, please use the command \`${prefix}rlsetup [platform (pc/xbox/ps)] [account]\``
+        },
+        "notLinkedOther": {
+            "title": "Account Not Linked",
+            "description": `Unfortunately that player has not yet linked their account. You can check their stats if you have their platform and account name/ID by using \`${prefix}rlrank [platform] [account]\``
+        },
+        "invalidPlatform": {
+            "title": "Invalid Platform",
+            "description": "The platform you specified was not a valid option. The valid options are `pc`, `xbox`, and `ps`"
+        },
+        "noPlayerSpecified": {
+            "title": "No Player Specified",
+            "description": `To use this command, you need to specify an account following the syntax \`${prefix}rlrank [platform] [account]\``
         }
     }
 
     // Build MessageEmbed
     const Embed = new Discord.MessageEmbed()
         .setColor(message.client.config.color)
-        .setFooter('ORLA', message.client.config.logo)
+        .setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
         .setTitle('Error: ' + methods[method].title)
         .setDescription(methods[method].description)
 
