97003d0fd816f2358f18bc60ce67c345caf0cd17
Finley Sherwood <me@itsf.in>
2020-10-01 05:57:47 +1000

    Add rlstats command

---
+++ commands/rocket league/rlstats.js
@@ -0,0 +1,118 @@
+module.exports = {
+	desc: 'checks rocket league account stats',
+	syntax: '[platform] [account]',
+	async run(message, args) {
+		const Discord = require('discord.js')
+		const moment = require('moment-timezone')
+
+		const requestInitiated = moment().unix()
+
+		const accountInfo = {
+			"isReal": false
+		}
+
+		if (!(0 in args)) {
+			// if checking own rank
+			const userSQL = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.author.id+'"')
+			const user = userSQL[0]
+
+			if (user.account !== null) {
+				// if account is linked
+				accountInfo['platform'] = user.platform
+				accountInfo['account'] = user.account
+				accountInfo['isReal'] = true
+			} else {
+				// if account is not linked
+				message.client.error('notLinked', message)
+				return
+			}
+		} else {
+			// if checking other player's rank
+			if (message.mentions.members.first()) {
+				// if checking via ping
+				const userSQL = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.mentions.members.first().user.id+'"')
+				const user = userSQL[0]
+
+				if (user.account !== null) {
+					// if account is linked
+					accountInfo['platform'] = user.platform
+					accountInfo['account'] = user.account
+				} else {
+					// if account is not linked
+					message.client.error('notLinkedOther', message)
+					return
+				}
+			} else {
+				// if checking via platform and account
+				if (['pc', 'ps', 'xbox'].indexOf(args[0].toLowerCase()) === -1) {
+                    message.client.error('invalidPlatform', message)
+                    return
+                }
+                
+                if (!(1 in args)) {
+                    message.client.error('noPlayerSpecified', message)
+                    return
+				}
+				
+				accountInfo['platform'] = args[0].toLowerCase()
+				accountInfo['account'] = args[1]
+			}
+		}
+
+		const loading = message.guild.emojis.cache.find(emoji => emoji.name === 'd_loading')
+
+		message.channel.send(`${loading} Getting stats...`).then(async msg => {
+			// Don't bother looking for stats.js, its in the .gitignore
+			const stats = require('../../utils/stats.js')
+
+			const playerRaw = await stats(accountInfo['platform'], accountInfo['account'])
+
+			if (playerRaw !== null) {
+				const player = JSON.parse(playerRaw)
+				
+				const Embed = new Discord.MessageEmbed()
+					.setColor(message.client.config.color)
+					.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+					.setTitle(`RL Stats: ${player.user.name}`)
+					.setURL(player.user.statsURL)
+					.setThumbnail(player.user.avatarURL)
+				
+				let totalModes = 0
+				let topRank = false
+				for (let i = 0; i < Object.keys(player.stats).length-1; i++) {
+					let stat = player.stats[Object.keys(player.stats)[i]]
+					
+					const percentile = `(Top ${Math.floor((100 - stat.percentile)*10)/10}%)`
+					const value = Number(Math.floor(stat.value*10)/10).toLocaleString('en')
+					const percentage = (stat.name === 'Goal Ratio') ? '%' : ''
+					const overall = Number(stat.overall).toLocaleString('en')
+
+					Embed.addField(`__${stat.name}__ *${percentile}*`, `${value}${percentage}\nGlobal: #${overall}`, true)
+				}
+
+				Embed.addField('First Season', player.stats.firstSeason, true)
+				Embed.addField("‎", "‎", true) // these have invisible characters in them
+
+				msg.edit(' ', Embed)
+			} else {
+				if (requestInitiated > (moment().unix() - 20)) {
+					const Embed = new Discord.MessageEmbed()
+						.setColor(message.client.config.color)
+						.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+						.setTitle('Error: Profile Not Found')
+						.setDescription('The profile you searched for could not be found. Please try again.')
+					
+					msg.edit(' ', Embed)
+				} else {
+					const Embed = new Discord.MessageEmbed()
+						.setColor(message.client.config.color)
+						.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+						.setTitle('Error: Request Timeout')
+						.setDescription('The server took too long to respond. Unfortunately we can\'t receive your account stats right now.')
+					
+					msg.edit(' ', Embed)
+				}
+			}
+		})
+	}
+}
