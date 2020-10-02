9ad3bfbfa51446fe54734544ee49d57a6498e8a8
Finley Sherwood <me@itsf.in>
2020-10-02 13:59:59 +1000

    Add message to xp command

--- commands/staff/xp.js
+++ commands/staff/xp.js
@@ -2,10 +2,19 @@ module.exports = {
 	desc: 'give or take a user\'s XP',
 	syntax: '[@user]* [amount]*',
 	async run(message, args) {
+		const Discord = require('discord.js')
+
 		// Only run command if the args are valid
 		if (isNaN(args[1])) return
 		if (message.mentions.members.first() == undefined) return
 
-		const newXP = await message.client.xp.giveXP(message, args[1])
+		await message.client.xp.giveXP(message, args[1])
+
+		const Embed = new Discord.MessageEmbed()
+			.setColor(message.client.config.color)
+			.setDescription(`You successfully gave ${message.mentions.members.first().user} ${args[1]} xp.`)
+			.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+		
+		message.channel.send(Embed)
     }
 }
