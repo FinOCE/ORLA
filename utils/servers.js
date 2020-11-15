9fde83997219f772983d059527023781d3f40950
Finley Sherwood <me@itsf.in>
2020-11-15 16:01:40 +1000

    Simplify unnecessary code in servers.js

--- utils/servers.js
+++ utils/servers.js
@@ -3,17 +3,9 @@ module.exports = async (client) => {
 
     const servers = {}
     for (const server of serverSQL) {
-        servers[server.id] = {
-            id: server.id,
-            timezone: server.timezone,
-            upcoming: server.upcoming,
-            message: server.message,
-            announcements: server.announcements,
-            notifications: server.notifications,
-            joinleave: server.joinleave,
-            autorole: JSON.parse(server.autorole),
-            pingrole: server.pingrole
-        }
+        servers[server.id] = server
+        servers[server.id].upcoming = JSON.parse(server.upcoming)
+        servers[server.id].autorole = JSON.parse(server.autorole)
     }
     
     return servers
