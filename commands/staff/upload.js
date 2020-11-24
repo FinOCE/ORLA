fad34df3ddd2f76c981fdbc5b669c922c84f0381
Finley Sherwood <me@itsf.in>
2020-11-24 20:55:26 +1000

    Determine if an event is open for registration when uploading

--- commands/staff/upload.js
+++ commands/staff/upload.js
@@ -5,7 +5,10 @@ module.exports = {
 		const {Event} = require('../../utils/Event')
 		const event = new Event()
 		
-		//event.name = await event.query(message, 'What is the name of the tournament?')
+		// ----- NAME ----
+		event.name = await event.query(message, 'What is the name of the tournament?')
+
+		// ----- HOST & SERIES -----
 		let validHost = false
 		while (validHost == false) {
 			const backend = await event.query(message, 'What is the series/host code?')
@@ -51,7 +54,6 @@ module.exports = {
 						message.channel.send('Sorry, but that option is not valid. Please try again.')
 					} else {
 						if (option-1 <= Object.keys(seriesToCheck).length) {
-							//console.log(seriesToCheck[Object.keys(seriesToCheck)[option-1]])
 							query = await message.client.query('SELECT * FROM `hosts` WHERE `series` LIKE "%'+Object.keys(seriesToCheck)[option-1]+'%"')
 							event.host = query.getFirst().name
 							event.series = Object.keys(seriesToCheck)[option-1]
@@ -67,16 +69,37 @@ module.exports = {
 				validHost = true
 			}
 		}
-		console.log(event.host, event.series)
-		return
 
+		// ----- URL -----
 		event.URL = await event.query(message, 'What is the URL to the registration page?')
-		event.customURL = await event.query(message, 'What do you want the custom URL to be for the ORLA redirect?') // if not in series
-		event.streamURL = await event.query(message, 'What is the URL to the stream page? If there is none, type anything except a link.')
-		event.prize = await event.query(message, 'What is the total prize pool for the event?')
-		event.open = await event.query(message, 'Is the event open for anyone to join? If so, type anything including the letter Y.')
 
-		message.channel.send(`ok`)
-		console.log(event)
+		// ----- CUSTOM URL -----
+		event.customURL = await event.query(message, 'What do you want the custom URL to be for the ORLA redirect?')
+
+		// ----- STREAM URL -----
+		event.streamURL = await event.query(message, 'What is the URL to the stream page? If there is none, type anything except a link.')
+
+		// ----- PRIZE -----
+		event.prize = await event.query(message, 'What is the total prize pool for the event?')
+
+		// ----- OPEN -----
+		const open = await event.query(message, 'Is the event open for anyone to join? If so, type anything including the letter Y.')
+		event.open = (open.toLowerCase().includes('y')) ? true : false
+
+		// ----- START TIME -----
+		validStartTime = false
+		while (validStartTime == false) {
+			startTime = await event.query(message, 'When does the event start? (format: D-M hh:mm, e.g. 16-3 18:00')
+			// TODO: convert to unix before setting
+		}
+
+		// ----- REGISTRATION TIME -----
+		validRegistrationTime = false
+		while (validRegistrationTime == false) {
+			registrationTime = await event.query(message, 'When does registration close? (format: D-M hh:mm, e.g. 16-3 18:00')
+			// TODO: copy valid conversion code from startTime part
+		}
+
+		// TODO: confirmation and upload
     }
 }
