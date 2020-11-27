26e6c197fb357c6348864251022803a47e313422
Finley Sherwood <me@itsf.in>
2020-11-27 18:22:30 +1000

    Rewrite to use the Database class object

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,19 @@
 module.exports = async (client) => {
     // Set status and show the bot is running
     console.log('Bot is now online')
-    client.user.setActivity('orla.pro | .help', { type: 'WATCHING' })
+    client.user.setActivity('orla.pro | .help', {type: 'WATCHING'})
 
     // Execute functions on an interval
     client.setInterval(function update() {
         // Create server list
         (async () => {
-            const serverSQL = await client.sql('SELECT * FROM `servers`')
-
-            const servers = {}
-            for (const server of serverSQL) {
-                servers[server.id] = server
-                servers[server.id].upcoming = JSON.parse(server.upcoming)
-                servers[server.id].autorole = JSON.parse(server.autorole)
-            }
-
-            client.servers = servers
+            const servers = await client.query('SELECT * FROM `servers`')
+            servers.getAll().forEach(server => {
+                server.upcoming = JSON.parse(server.upcoming)
+                server.autorole = JSON.parse(server.autorole)
+                server.rankroles = JSON.parse(server.rankroles)
+            })
+            client.servers = servers.getAll()
         })()
 
         return update
