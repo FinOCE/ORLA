23110a396054eec0f7a7cad1bd3526e3f184f3ed
Finley Sherwood <me@itsf.in>
2020-12-12 13:55:54 +1000

    Use Role.js class to handle role reactions

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,5 +1,12 @@
 module.exports = async (client, reaction, user) => {
     if (user.bot) return
-
-    client.roles.addStateRole(client, reaction, user)
+    
+    let manualrole = await client.query('SELECT `manualrole` FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
+    if (manualrole !== null) {
+        manualrole = JSON.parse(manualrole)
+        if (reaction.message.channel.id === manualrole.channelID) {
+            const {Role} = require('../utils/Role')
+            new Role(client, reaction, user).add(manualrole)
+        }
+    }
 }
---
+++ utils/Role.js
@@ -0,0 +1,13 @@
+module.exports.Role = class Role {
+    constructor(client, reaction, user) {
+        this.client = client
+        this.reaction = reaction
+        this.user = user
+    }
+    add(manualrole) {
+        // Add role
+    }
+    remove(manualrole) {
+        // Remove role
+    }
+}
