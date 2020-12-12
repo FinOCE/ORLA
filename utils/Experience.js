dc2eeca6fc6819cd1cfcf3a27a852fa18c31b20a
Finley Sherwood <me@itsf.in>
2020-12-12 12:20:35 +1000

    Begin function to give roles based off XP

--- utils/Experience.js
+++ utils/Experience.js
@@ -31,5 +31,27 @@ module.exports.Experience = class Experience {
     }
     update(message) {
         // Update roles here
+        let xproles = await message.client.query('SELECT `xproles` FROM `servers` WHERE `id`="'+message.guild.id+'"')
+        if (xproles !== null) {
+            xproles = JSON.parse(xproles) // = list(lvl1, lvl2, lvl3) where lvl = role snowflake
+        }
+
+        /* ----- OLD -----
+
+        let {xproles} = await message.client.sql('SELECT `xproles` FROM `servers` WHERE `id`="'+message.guild.id+'"')
+        if (xproles !== null && xproles !== undefined) {
+            roles = JSON.parse(xproles)
+
+            const users = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.author.id+'"')
+            const level = this.getLevel(users[0].xp)[0]
+
+            for (i = 0; i < Object.keys(roles).length; i++) {
+                if ((level >= Object.keys(roles)[i]) && (level < Object.keys(roles)[i+1])) {
+                    message.member.roles.add(roles[Object.keys(roles)[i]])
+                    if (i != 0) message.member.roles.remove(roles[Object.keys(roles)[i-1]])
+                }
+            }
+        }
+        */
     }
 }
