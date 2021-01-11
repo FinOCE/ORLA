bcb8d0a18a03e8d1472c36aa1e5afa6236fed0ac
Finley Sherwood <me@itsf.in>
2021-01-11 12:39:12 +1000

    Update userinfo to use new User object

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -4,37 +4,39 @@ module.exports = {
     async run(message, args) {
         const Discord = require('discord.js')
         const moment = require('moment-timezone')
-
-        const userSQL = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+message.author.id+'"')
-        const timezone = userSQL[0].timezone
-
+        
+        // Create User object
+        const {User} = require('../../utils/User')
+        const user = await User.build(message.client, (message.mentions.members.first()) ? message.mentions.members.first().user.id : message.author.id)
         const member = (message.mentions.members.first()) ? message.mentions.members.first() : message.member
-        const displayName = (member.nickname === null) ? member.user.username : member.nickname
 
-        let user = await message.client.sql('SELECT * FROM `users` WHERE `id`="'+member.user.id+'"')
-        if (!(0 in user)) return
-        user = user[0]
-
-        const total = user.xp
-        const level = message.client.xp.getLevel(total)[0]
-        const rankup = message.client.xp.getLevel(total)[1]
-        const remainder = message.client.xp.getLevel(total)[2]
-        const position = await message.client.sql('SELECT * FROM `users` WHERE `xp`>'+total)
-
+        // Create Embed object
         const Embed = new Discord.MessageEmbed()
             .setColor(message.client.config.color)
-            .setTitle(`Account Info: ${displayName}`)
+            .setTitle(`Account Info: ${user.discord.username}#${user.discord.discriminator}`)
             .setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+            .setThumbnail(user.discord.avatarURL)
         
-        if ((user.url !== null) && (user.url !== undefined)) Embed.setDescription(`https://orla.pro/${user.url}`)
+        // Add ORLA link for description
+        if (user.orla.url !== null) Embed.setDescription(user.orla.url)
 
-        // Add line from emojis here
+        // Add XP widget
+        let position = await message.client.query('SELECT * FROM `users` WHERE `xp`>'+user.orla.xp.total)
+        position = position.getAll()
         const line = ''
+        Embed.addField('Discord XP', `Level: **${user.orla.xp.level}**\n${line}\nTotal: **${user.orla.xp.total}** - **${user.orla.xp.progress.remaining}**/**${user.orla.xp.progress.cost}**\nPosition: **#${position.length+1}**`, true)
 
-        Embed.addField('Discord XP', `Level: **${level}**\n${line}\nTotal: **${total}** - **${remainder}**/**${rankup}**\nPosition: **#${position.length+1}**`, true)
+        // Add platform widget
+        let platformField = ''
+        Object.keys(user.platforms).forEach(platform => {
+            if (user.platforms[platform] && platform !== 'main') {
+                const logo = message.client.guilds.cache.find(g => g.id === '690588183683006465').emojis.cache.find(e => e.name === `b_${platform}`)
+                platformField += `${logo} **${user.platforms[platform]}**\n`
+            }
+        })
+        Embed.addField('Accounts', platformField, true)
 
-        // Add platforms here as embed
-
+        // Add role widget
         const roles = member._roles.map(role => `<@&${role}>`)
         let roleField = ''
         roles.forEach(role => {
@@ -42,9 +44,12 @@ module.exports = {
         })
         Embed.addField('Roles', roleField, false)
 
-        Embed.addField('Account Created', moment.unix(Math.round(member.user.createdAt/1000)).tz(timezone).format('Do MMMM YYYY, h:mma z'), true)
-        Embed.addField('Joined Server', moment.unix(Math.round(member.joinedTimestamp/1000)).tz(timezone).format('Do MMMM YYYY, h:mma z'), true)
+        // Add key date widgets
+        /* ----- CHECK WHY FIRST IS NOT DOING DAYLIGHT SAVINGS, UPDATE TO USE USER'S TIMEZONE ----- */
+        Embed.addField('Account Created', moment.unix(Math.round(user.discord.createdAt/1000)).tz('Australia/Brisbane').format('Do MMMM YYYY, h:mma z'), true)
+        Embed.addField('Joined Server', moment.unix(Math.round(member.joinedTimestamp/1000)).tz('Australia/Brisbane').format('Do MMMM YYYY, h:mma z'), true)
 
+        // Send Embed
         message.channel.send(Embed)
     }
 }
