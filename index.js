c2bb546da87db2271fce398176245fba8e50687a
Finley Sherwood <me@itsf.in>
2021-02-04 08:43:02 +1000

    Update formatting

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js')
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 client.config = require('./config.json')
 client.error = require('./utils/error.js')
+
+// SQL Function
 const {Database} = require('./utils/Database')
 client.query = (sql) => Database.query(sql)
 
@@ -17,10 +19,9 @@ const { parse } = require('path')
 // Load events
 glob('./events/*.js', (_, files) => {
     files.forEach(file => {
-        const { name } = parse(file)
+        const {name} = parse(file)
         
         const event = require(file)
-        
         client.on(name, event.bind(null, client))
     })
 })
@@ -29,13 +30,12 @@ glob('./events/*.js', (_, files) => {
 client.commands = new Discord.Collection()
 glob('./commands/**/*.js', (_, files) => {
     files.forEach(file => {
-        const { dir, name } = parse(file)
+        const {dir, name} = parse(file)
         const category = dir.split('/').pop()
         
         const command = require(file)
         command.category = category
         command.name = name
-        
         client.commands.set(name, command)
     })
 })
