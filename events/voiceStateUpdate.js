899bad8bc607282f33f00bb5e2ea9d0786420f0b
Finley Sherwood <me@itsf.in>
2021-02-04 08:42:02 +1000

    Disable faulty XP system

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,9 @@
 module.exports = (client, from, to) => {
-    if (to.channelID !== null && to.channelID !== client.servers[to.guild.id].afk) {
+    /*if (to.channelID !== null && to.channelID !== client.servers[to.guild.id].afk) {
         client.xpEarntVC[to.id] = client.setInterval(() => {
             client.xp.addXP(to, to.id, 6, 5)
         }, 60000)
     } else {
         clearInterval(client.xpEarntVC[to.id])
-    }
+    }*/
 }
