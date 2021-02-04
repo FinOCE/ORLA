412029d92a589bdac9c43739e69a808f13d6caf0
Finley Sherwood <me@itsf.in>
2021-02-04 11:24:10 +1000

    Fix member joining crash

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,8 @@
 module.exports = (client, member) => {
-    //member.roles.add(client.config.COMMUNITY)
-    //member.roles.add(client.config.ALPHA)
-    for (const i in client.servers[member.guild].autorole) {
-        member.roles.add(client.servers[member.guild].autorole[i])
+    for (const i in client.servers.find(s => s.id === member.guild.id).autorole) {
+        member.roles.add(client.servers.find(s => s.id === member.guild.id).autorole[i])
     }
     
-    const channel = client.channels.cache.find(channels => channels.id === client.servers[member.guild].joinleave)
+    const channel = client.channels.cache.find(channels => channels.id === client.servers.find(s => s.id === member.guild.id).joinleave)
     channel.send(`<@!${member.user.id}> has **joined** the server`)
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
 module.exports = (client, member) => {
-    const channel = client.channels.cache.find(channels => channels.id === client.servers[member.guild].joinleave)
+    const channel = client.channels.cache.find(channels => channels.id === client.servers.find(s => s.id === member.guild.id).joinleave)
     channel.send(`<@!${member.user.id}> has **left** the server`)
 }
