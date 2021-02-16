89e4c8a0e615c2aba1cbf659690675236f4bd0f1
Finley Sherwood <me@itsf.in>
2021-02-16 11:53:22 +1000

    Update to use User object

--- commands/xp/leaderboard.js
+++ commands/xp/leaderboard.js
@@ -7,23 +7,28 @@ module.exports = {
 
 		const page = (0 in args && !isNaN(args[0]) && args[0] > 0) ? args[0]-1 : 0
 
-		const users = (await message.client.query('SELECT * FROM `users`')).getAll()
-		users.sort((a, b) => {return b.xp - a.xp})
+		const loading = message.client.guilds.cache.find(g => g.id === '690588183683006465').emojis.cache.find(e => e.name === 'd_loading')
+		message.channel.send(`${loading} Getting leaderboard...`).then(async msg => {
+			const users = (await message.client.query('SELECT * FROM `users`')).getAll()
+			users.sort((a, b) => {return b.xp - a.xp})
 
-		const Embed = new Discord.MessageEmbed()
-			.setColor(message.client.config.color)
-			.setTitle(`XP Leaderboard`)
-			.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
-		
-		let list = ''
+			const Embed = new Discord.MessageEmbed()
+				.setColor(message.client.config.color)
+				.setTitle(`XP Leaderboard`)
+				.setFooter(`ORLA - Requested by ${message.author.tag}`, message.client.config.logo)
+			
+			let list = ''
 
-		let range = (users.length - page * 10 < 10) ? users.length - page * 10 : 10
-		for (let x = 0; x < range; x++) {
-			list += `**#${page * 10 + x + 1}** <@${users[page*10+x].id}> - Level **${message.client.xp.getLevel(users[page*10+x].xp)[0]}** (**${users[page*10+x].xp}**)\n`
-		}
+			let range = (users.length - page * 10 < 10) ? users.length - page * 10 : 10
+			for (let i = 0; i < range; i++) {
+				const {User} = require('../../utils/User')
+				const user = await User.build(message.client, users[page*10+i].id)
+				list += `**#${page * 10 + i + 1}** <@${user.id}> - Level **${user.orla.xp.level}** (**${user.orla.xp.total}**)\n`
+			}
 
-		Embed.setDescription(`Page **${page + 1}** of **${Math.ceil(users.length/10)}**\n\n${list}`)
+			Embed.setDescription(`Page **${page + 1}** of **${Math.ceil(users.length/10)}**\n\n${list}`)
 
-		if (list !== '') message.channel.send(Embed)
+			msg.edit(' ', Embed)
+		})
     }
 }
