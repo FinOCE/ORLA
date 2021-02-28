5c299879b81bd8ff7b7101411515a6d86b94aedf
Finley Sherwood <me@itsf.in>
2021-02-28 23:28:29 +1000

    Clean up self-roles code

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,31 +1,26 @@
 module.exports = async (client, reaction, user) => {
     if (user.bot || reaction.message.guild.id !== client.config.mainServer) return
-    if (reaction.message.partial) {
-        await reaction.message.fetch()
-    }
-    
-    let query = await client.query('SELECT * FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
-    const server = query.getFirst()
+    if (reaction.message.partial) await reaction.message.fetch()
 
-    if (server.manualrole !== null) {
-        if (reaction.message.channel.id === JSON.parse(server.manualrole).channelID) {
-            if (['a_wa', 'a_vic', 'a_tas', 'a_sa', 'a_qld', 'a_nz', 'a_nt', 'a_nsw', 'a_act'].indexOf(reaction._emoji.name) !== -1) {
-                const role = reaction._emoji.name.replace('a_', '')
+    const server = client.servers[reaction.message.guild.id]
+    const stateRoles = server.stateroles
 
-                reaction.message.reactions.cache.forEach(emote => {
-                    if (emote._emoji.id !== reaction._emoji.id) {
-                        emote.users.remove(user.id)
-                        reaction.message.guild.member(user.id).roles.remove(JSON.parse(server.stateroles)[emote._emoji.name.replace('a_', '')])
-                    }
-                })
-                reaction.message.guild.member(user.id).roles.add(JSON.parse(server.stateroles)[role])
-                
-                await client.query('UPDATE `users` SET `location`="'+role+'" WHERE `id`="'+user.id+'"')
-            }
-            if (reaction._emoji.name === 'd_check') {
-                reaction.message.guild.member(user.id).roles.remove('690593520343449621')
-                reaction.message.guild.member(user.id).roles.add('745583174180012083')
-            }
-        }
+    if (reaction.message.channel.id !== server.manualrole.channelID) return
+    if (reaction.message.id === server.manualrole.stateMessageID) {
+        // State role reaction
+        // Remove other reactions
+        reaction.message.reactions.cache
+            .filter(r => r._emoji.id !== reaction._emoji.id)
+            .forEach(r => r.users.remove(user.id))
+        // Remove other roles and add chosen role
+        reaction.message.guild.member(user.id).roles.remove(Object.values(stateRoles)).then(() => {
+            reaction.message.guild.member(user.id).roles.add(server.stateroles[reaction._emoji.name.replace('a_', '')])
+        })
+        // Update SQL database
+        await client.query('UPDATE `users` SET `location`="'+reaction._emoji.name.replace('a_', '')+'" WHERE `id`="'+user.id+'"')
+    } else if (reaction.message.id === server.manualrole.muteMessageID) {
+        // Mute role reaction
+        reaction.message.guild.member(user.id).roles.remove('690593520343449621')
+        reaction.message.guild.member(user.id).roles.add('745583174180012083')
     }
 }
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,48 +1,20 @@
 module.exports = async (client, reaction, user) => {
     if (user.bot || reaction.message.guild.id !== client.config.mainServer) return
-    if (reaction.message.partial) {
-        await reaction.message.fetch()
-    }
-    
-    let query = await client.query('SELECT * FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
-    const server = query.getFirst()
+    if (reaction.message.partial) await reaction.message.fetch()
 
-    if (server.manualrole !== null) {
-        if (reaction.message.channel.id === JSON.parse(server.manualrole).channelID) {
-            if (['a_wa', 'a_vic', 'a_tas', 'a_sa', 'a_qld', 'a_nz', 'a_nt', 'a_nsw', 'a_act'].indexOf(reaction._emoji.name) !== -1) {
-                const role = reaction._emoji.name.replace('a_', '')
-                
-                reaction.message.guild.member(user.id).roles.remove(JSON.parse(server.stateroles)[role])
-                await client.query('UPDATE `users` SET `location`=null WHERE `id`="'+user.id+'"')
-            }
-            if (reaction._emoji.name === 'd_check') {
-                reaction.message.guild.member(user.id).roles.remove('745583174180012083')
-                reaction.message.guild.member(user.id).roles.add('690593520343449621')
-            }
-        }
+    const server = client.servers[reaction.message.guild.id]
+    const stateRoles = server.stateroles
+
+    if (reaction.message.channel.id !== server.manualrole.channelID) return
+    if (reaction.message.id === server.manualrole.stateMessageID) {
+        // State role reaction
+        // Remove chosen role
+        reaction.message.guild.member(user.id).roles.remove(server.stateroles[reaction._emoji.name.replace('a_', '')])
+        // Update SQL database
+        await client.query('UPDATE `users` SET `location`=null WHERE `id`="'+user.id+'"')
+    } else if (reaction.message.id === server.manualrole.muteMessageID) {
+        // Mute role reaction
+        reaction.message.guild.member(user.id).roles.remove('745583174180012083')
+        reaction.message.guild.member(user.id).roles.add('690593520343449621')
     }
 }
-/*module.exports = async (client, reaction, user) => {
-    if (user.bot) return
-    
-    let manualrole = await client.query('SELECT `manualrole` FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
-    manualrole = JSON.parse(manualrole.getFirst())
-    if (manualrole !== null) {
-        if (reaction.message.channel.id === manualrole.channelID) {
-            if (reaction.message.guild.id === '690588183683006465') {
-                // Only works for ORLA server
-                if (['a_wa', 'a_vic', 'a_tas', 'a_sa', 'a_qld', 'a_nz', 'a_nt', 'a_nsw', 'a_act'].indexOf(reaction._emoji.name) !== -1) {
-                    const role = reaction._emoji.name.replace('a_', '')
-                    const roles = await client.query('SELECT `stateroles` FROM `servers` WHERE `id`="'+reaction.message.guild.id+'"')
-
-                    reaction.message.guild.member(user.id).roles.remove(JSON.parse(roles.getAll())[role])
-                    await client.query('UPDATE `users` SET `location`=null WHERE `id`="'+user.id+'"')
-                }
-                if (reaction._emoji.name === 'd_check') {
-                    reaction.message.guild.member(user.id).roles.remove('745583174180012083')
-                    reaction.message.guild.member(user.id).roles.add('690593520343449621')
-                }
-            }
-        }
-    }
-}*/
