20bc0d1e4e978fd1b2991902204c2a7635a291b3
Finley Sherwood <me@itsf.in>
2021-03-04 15:12:14 +1000

    Rewrite in TypeScript

---
+++ commands/general/pfp.ts
@@ -0,0 +1,20 @@
+import Command, {CommandOptions} from '../../utils/Command'
+import {Message, MessageEmbed, GuildMember} from 'discord.js';
+import Client from '../../utils/Client'
+
+export default class PfpCommand extends Command {
+    constructor(client: Client, options: CommandOptions) {
+        super(client, options)
+    }
+
+    run(message: Message, args: Array<string>) {
+        if (!message.member) return
+        const member: GuildMember = message.mentions.members?.first() || message.member
+        const Embed: MessageEmbed = new MessageEmbed()
+            .setTitle(`${member.user.tag}`)
+            .setImage(member.user.avatarURL() || '')
+            .setColor(this.client.config.color)
+            .setFooter(`ORLA - Requested by ${message.author.tag}`, this.client.config.logo)
+        message.channel.send(Embed)
+    }
+}
