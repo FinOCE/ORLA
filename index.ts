7a1b41db502de2a8be849adf8d67ac50facac711
Finley Sherwood <me@itsf.in>
2021-03-04 14:21:07 +1000

    Move regitrys to Client

--- index.ts
+++ index.ts
@@ -2,34 +2,6 @@
 import Client from './utils/Client'
 const client: Client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 
-// Import modules
-require('dotenv').config()
-const {glob} = require('glob')
-import {parse} from 'path'
-
-// Load events
-glob('./events/+(*.js|*.ts)', (err: string, files: Array<string>) => {
-    if (err) throw err
-    files.forEach(file => {
-        const {name} = parse(file)
-        
-        const event = require(file)
-        client.on(name, event.bind(null, client))
-    })
-})
-
-// Load commands
-glob('./commands/**/+(*.js|*.ts)', (err: string, files: Array<string>) => {
-    if (err) throw err
-    files.forEach(file => {
-        const {dir, name} = parse(file)
-        const category = dir.split('/').pop()
-        
-        const command = require(file)
-        Object.assign(command, {category, name})
-        client.commands.set(name, command)
-    })
-})
-
 // Login
+require('dotenv').config()
 client.login(process.env.token)
