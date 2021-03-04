3de2abe17b3e63438236873c21f58fbb7f6f164c
Finley Sherwood <me@itsf.in>
2021-03-05 01:07:34 +1000

    Rewrite Exit command in TS

--- commands/staff/exit.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	desc: 'shuts down the bot',
-	syntax: '',
-	onlyORLA: true,
-	run(message, args) {
-        process.exit(1)
-	}
-}
---
+++ commands/staff/exit.ts
@@ -0,0 +1,13 @@
+import Command, {CommandOptions} from '../../utils/Command'
+import {Message} from 'discord.js';
+import Client from '../../utils/Client'
+
+export default class PfpCommand extends Command {
+    constructor(client: Client, options: CommandOptions) {
+        super(client, options)
+    }
+
+    run(message: Message, args: Array<string>) {
+        process.exit()
+    }
+}
