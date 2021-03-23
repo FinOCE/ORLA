99065a775e0379c2e617d1d3f8ea922068dd92ea
Finley Sherwood <me@itsf.in>
2021-03-23 11:58:57 +1000

    Rewrite say command to match edit command

--- commands/staff/say.ts
+++ commands/staff/say.ts
@@ -1,5 +1,5 @@
 import Command, {CommandOptions} from '../../utils/Command'
-import {Message, TextChannel, MessageEmbed} from 'discord.js';
+import {Message, TextChannel, MessageEmbed, Snowflake} from 'discord.js';
 import Client from '../../utils/Client'
 
 export default class SayCommand extends Command {
@@ -7,42 +7,34 @@ export default class SayCommand extends Command {
         super(client, options)
     }
 
-    run(message: Message, args: Array<string>) {
-        // Ignore if no args are sent
-        if (!args[0]) return
+    async run(message: Message, args: Array<string>) {
+        if (!args[0]) return this.client.error('invalidSyntax', message).send()
+
+		// Get channel if specified
+        const validChannel = (/[0-9]{18}/.test(args[0])) ? await message.client.channels.fetch(args[0]) : false
+		const channel = (validChannel) ? validChannel : message.channel
 
         // Create Embed
         const Embed = new MessageEmbed()
             .setColor(this.client.config.color)
-        
-        // Redirect to new channel if provided
-        if (args[0].startsWith('<#') && args[0].endsWith('>')) {
-            let newChannelID = args[0].replace('<#','').replace('>','')
-            var channel = message.client.channels.cache.find(channels => channels.id === newChannelID)
-            args.shift()
-        }
 
         // Add image if provided
         if (args[0].startsWith('http')) {
             Embed.setImage(args[0])
             args.shift()
         }
-
+		
         // Split title and message
-        let response = args.join(' ')
-        if (response.includes('|')) {
-            let content = response.split('|')
-            Embed.setTitle(content[0])
-            content.shift()
-            response = content.join('|')
-        }
-        Embed.setDescription(response)
-
-        // Send to correct channel
-        if (channel) {
-            (channel as TextChannel).send(Embed)
+        const content = args.join(' ')
+        if (content.includes('|')) {
+            const title = content.split('|')[0]
+            const description = content.split('|').slice(1, content.length-1).join('|')
+            Embed.setTitle(title)
+            Embed.setDescription(description)
         } else {
-            message.channel.send(Embed)
+            Embed.setDescription(content)
         }
+
+		(channel as TextChannel).send(Embed)
     }
 }
